3a98eb9e69e7c7ae892b1e26d4da816a2fe435d6
Rafael-Dpaula <rafaeldepaula939@gmail.com>
2026-09-08 22:26:57 -0300

    Refatora o controlador de itens do cardápio para melhorar a formatação e a legibilidade do código

--- controler/itens_cardapio_controler.py
+++ controler/itens_cardapio_controler.py
@@ -1,17 +1,22 @@
 from dao.cardapio_dao import CardapioDAO
 
+
 class ItensCardapioControler:
-    
+
     def __init__(self):
         self.dao = CardapioDAO()
 
     def listar_itens_cardapio(self):
         return self.dao.select()
+
     def buscar_item_cardapio(self, id_item):
         return self.dao.selectID(id_item)
+
     def adicionar_item_cardapio(self, item):
         return self.dao.insert(item)
+
     def atualizar_item_cardapio(self, id_item, item):
-        return self.dao.update  (id_item, item)
+        return self.dao.update(id_item, item)
+
     def remover_item_cardapio(self, id_item):
         return self.dao.delete(id_item)
